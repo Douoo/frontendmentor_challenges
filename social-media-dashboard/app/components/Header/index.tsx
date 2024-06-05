b80a618e718f96b3d33d75461b44172728a1ab4e
Douoo <biruksewberiso@gmail.com>
2024-06-05 13:06:24 +0300

    Refactored code

--- social-media-dashboard/app/components/Header/index.tsx
+++ social-media-dashboard/app/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const ThemeToggle = dynamic(() => import("../ThemeToggle"), {
+const ThemeToggle = dynamic(() => import("./ThemeToggle"), {
   ssr: false,
 });
 
